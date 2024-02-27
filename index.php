225a09b68be28b278c5a869d1d99cdf24d209a87
SerenaMalusa <sere.malu@gmail.com>
2024-02-27 17:23:18 +0100

    feat: filter for parking

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 
 require_once __DIR__ ."./data/init.php";
 
-$filterd_hotels = [];
+$filterd_hotels = $hotels;
 
-var_dump($_GET);
+$user_parking = isset($_GET["parking"]) ? true : false;
+
+if ($user_parking) {
+    
+    $filterd_hotels = array_filter($filterd_hotels, function($hotel, $key) {
+        return $hotel['parking'] == true;
+    }, ARRAY_FILTER_USE_BOTH);
+
+} ;
+
+var_dump ($filterd_hotels);
 
 ?>
 
