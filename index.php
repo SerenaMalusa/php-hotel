729dc9b0211e16eff632c51cd62d701a9a5c9c24
SerenaMalusa <sere.malu@gmail.com>
2024-02-27 16:38:42 +0100

    feat: connect files and save data

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+
+include_once __DIR__ ."./data/init.php";
+
+var_dump($hotels);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <!-- characters and viewport tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- SEO tags -->
+    <meta name="author" content="Serena Malusà" />
+    <meta name="description" content="php exercise">
+
+    <!-- title and icon -->
+    <title>PHP Hotels</title>
+    <link rel="icon" href="./imgs/logo-small.ico">
+
+    <!-- CSS -->
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    
+</body>
+</html>
