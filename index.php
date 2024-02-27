4e80e0c399d8af228af75692c6ce7c50c2d2a59b
SerenaMalusa <sere.malu@gmail.com>
2024-02-27 16:46:59 +0100

    feat: show all elements in page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ ."./data/init.php";
 
-var_dump($hotels);
+// var_dump($hotels);
 
 ?>
 
@@ -28,6 +28,29 @@ var_dump($hotels);
 
 </head>
 <body>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Parking</th>
+            <th scope="col">Vote</th>
+            <th scope="col">Distance from City center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel): ?>
+            <tr>
+            <td><?= $hotel['name'] ?></td>
+            <td><?= $hotel['description'] ?></td>
+            <td><?= $hotel['parking'] ?></td>
+            <td><?= $hotel['vote'] ?></td>
+            <td><?= $hotel['distance_to_center'] ?></td>
+            </tr>
+            <? endforeach; ?>
+        </tbody>
+    </table>
     
 </body>
 </html>
